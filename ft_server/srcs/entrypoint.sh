b0556d44e20d5bad737feaf15087b7fa099a49bc
최원혁 <wonchoi@choewonhyeog-ui-MacBookPro.local>
2021-02-28 21:47:26 +0900

    server

---
+++ ft_server/srcs/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+service mysql start
+echo "CREATE DATABASE IF NOT EXISTS wordpress;" \
+	| mysql -u root --skip-password
+echo "CREATE USER IF NOT EXISTS 'wordpress'@'localhost' IDENTIFIED BY 'wordpress';" \
+	| mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' WITH GRANT OPTION;" \
+	| mysql -u root --skip-password
+if [ "$AUTOINDEX" == "off" ]; then
+	sed -i "60s/.*/		#autoindex on;/g" /etc/nginx/sites-available/default
+fi
+
+service nginx start
+service php7.3-fpm start
+service mysql restart
+rm /tmp/*
+
+bash

3355fbf18f0f9d084f8b124ee85897ae8de70d73
최원혁 <wonchoi@choewonhyeog-ui-MacBookPro.local>
2021-06-14 01:23:00 +0900

    cub3d 120

--- cub3d/includes/main.h
+++ cub3d/includes/main.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taewakim <taewakim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/11 17:20:32 by wonchoi           #+#    #+#             */
+/*   Updated: 2021/06/12 02:47:26 by taewakim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MAIN_H
 # define MAIN_H
 
@@ -7,7 +19,7 @@
 
 # include "raycasting.h"
 
-char	*setting_program(t_cub3d **cub3d, char *map_path);
+char	*setting_program(t_cub3d *cub3d, char *map_path);
 void	free_cub3d(t_cub3d *cub3d);
 
 #endif

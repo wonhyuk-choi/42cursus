711f25f076df33a2a8c2e68cd0d4acbf73e6ead9
최원혁 <wonchoi@choewonhyeog-ui-MacBookPro.local>
2021-06-15 15:02:17 +0900

    ftactol mlx

---
+++ fractol/includes/fractol.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wonchoi <wonchoi@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/14 01:42:08 by wonchoi           #+#    #+#             */
+/*   Updated: 2021/06/14 01:43:43 by wonchoi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef "FRACTOL_H"
+# define "FRACTOL_H"
+
+# include <math.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include "mlx.h"
+
+
+#endif

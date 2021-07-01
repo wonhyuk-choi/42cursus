307a1d677b1184fd9e713126329efaf08d31c8c0
Wonhyuk Choi <wonchoi@c10r6s8.42seoul.kr>
2021-07-01 17:46:08 +0900

    phli

--- philo/main.c
+++ philo/main.c
@@ -6,15 +6,22 @@
 /*   By: wonchoi <wonchoi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 17:27:44 by wonchoi           #+#    #+#             */
-/*   Updated: 2021/07/01 17:28:35 by wonchoi          ###   ########.fr       */
+/*   Updated: 2021/07/01 17:33:51 by wonchoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 int	main(int argc, char **argv)
 {
+	if (argc == 6 || argc == 7)
+	{
+
+	}
+	else
+		return (-1);
 	return (0);
 }
